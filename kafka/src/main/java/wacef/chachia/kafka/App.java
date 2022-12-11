8f55289129b3d8e2dd67ab59ddadc72888cab941
Mohamed Wacef <63343846+chachia-wacef@users.noreply.github.com>
2022-12-11 12:46:45 +0100

    Add files via upload

---
+++ kafka/src/main/java/wacef/chachia/kafka/App.java
@@ -0,0 +1,7 @@
+package wacef.chachia.kafka;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello Wacef, this is you kafka-spark integration project");
+    }
+}
---
+++ spark/src/main/java/wacef/chachia/spark/App.java
@@ -0,0 +1,7 @@
+package wacef.chachia.spark;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello wacef, you spark project is launched");
+    }
+}
